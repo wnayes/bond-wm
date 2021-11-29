12fedf5c62b496b3f3ec733edcecab485d9fa93b
Will Nayes <wnayes@gmail.com>
2021-11-29 16:43:43 -0600

    Set up esbuild for renderer-desktop.

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useCallback, useEffect, useRef } from "react";
 
-import * as moment from "moment";
+import moment from "moment";
 
 export function Clock() {
   const divRef = useRef<HTMLDivElement>();
