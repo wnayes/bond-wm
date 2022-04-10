57ef161bd364acdd9de13df8b7f757a664f0a944
Will Nayes <wnayes@gmail.com>
2022-04-10 11:30:15 -0500

    Fix RGBA icon data handling.

--- renderer-shared/hooks.ts
+++ renderer-shared/hooks.ts
@@ -36,10 +36,10 @@ export function useIconInfoDataUri(iconInfo: IIconInfo): string | undefined {
     const iconImageData = context.createImageData(iconInfo.width, iconInfo.height);
     const iconData = iconInfo.data;
     for (let i = 0; i < iconData.length; i++) {
-      iconImageData.data[i * 4 + 1] = iconData[i] & 0xff;
-      iconImageData.data[i * 4 + 2] = (iconData[i] >>> 8) & 0xff;
-      iconImageData.data[i * 4 + 3] = (iconData[i] >>> 16) & 0xff;
-      iconImageData.data[i * 4 + 0] = (iconData[i] >>> 24) & 0xff;
+      iconImageData.data[i * 4 + 0] = (iconData[i] >>> 16) & 0xff; // R
+      iconImageData.data[i * 4 + 1] = (iconData[i] >>> 8) & 0xff; // G
+      iconImageData.data[i * 4 + 2] = iconData[i] & 0xff; // B
+      iconImageData.data[i * 4 + 3] = (iconData[i] >>> 24) & 0xff; // A
     }
     context.putImageData(iconImageData, 0, 0);
     setDataUri(canvas.toDataURL());
