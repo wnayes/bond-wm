c134d9295568abe11dbeaa6ea5ce096afb77e6b9
Will Nayes <wnayes@gmail.com>
2022-10-09 14:25:22 -0500

    Refactor Wallpaper component logic.

--- renderer-desktop/components/Wallpaper.tsx
+++ renderer-desktop/components/Wallpaper.tsx
@@ -10,11 +10,7 @@ export function Wallpaper() {
   const { width, height } = useWindowSize();
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) {
-      return;
-    }
-    const ctx = canvas.getContext("2d");
+    const ctx = canvasRef.current?.getContext("2d");
     if (!ctx) {
       return;
     }
@@ -38,46 +34,97 @@ export function Wallpaper() {
     // and applies only to this algorithm.
 
     // line segments (either few, or fluent lines (200))
-    const segments = Math.random() < 0.5 ? 1 + Math.floor(9 * Math.random()) : 200;
-    const wavelength = width / (5 + 15 * Math.random());
+    const seed = generateWallpaperSeed(width);
 
-    const layers = 3 + Math.floor(10 * Math.random());
-    const hueStart = 360 * Math.random();
-    const hueIncrement = 20 - 40 * Math.random();
-    const ampl = 0.1 * wavelength + 0.9 * wavelength * Math.random();
-    const offset = width * Math.random();
-    const offsetIncrement = width / 20 + (width / 10) * Math.random();
-    const sat = 15 + 35 * Math.random();
-    const light = 15 + 45 * Math.random();
-    const lightIncrement = Math.random() < 0.5 ? 2 + 4 * Math.random() : -(2 + 4 * Math.random());
-
-    ctx.fillStyle = "hsl( " + hueStart + ", " + sat + "%, " + light + "% )";
-    ctx.fillRect(0, 0, width, height);
-
-    for (let l = 0; l < layers; l++) {
-      const h = hueStart + (l + 1) * hueIncrement;
-      const s = sat;
-      const v = light + (l + 1) * lightIncrement;
-      ctx.fillStyle = "hsl( " + h + ", " + s + "%, " + v + "% )";
-      ctx.beginPath();
-      const layerOffset = offset + offsetIncrement * l;
-      const offsetY = (l + 0.5) * (height / layers);
-      const startY = offsetY + ampl * Math.sin(layerOffset / wavelength);
-      ctx.moveTo(0, startY);
-      for (let i = 0; i <= segments; i++) {
-        const x = i * (width / segments);
-        ctx.lineTo(x, startY + ampl * Math.sin((layerOffset + x) / wavelength));
-      }
-      ctx.lineTo(width, height);
-      ctx.lineTo(0, height);
-      ctx.lineTo(0, startY);
-      ctx.fill();
-    }
+    paintWallpaper(ctx, seed, width, height);
 
     lastImageData.current = ctx.getImageData(0, 0, width, height);
   }, [width, height]);
 
   return <canvas className="wallpaper" ref={canvasRef} width={width} height={height}></canvas>;
+}
+
+interface IWallpaperSeed {
+  segments: number;
+  wavelength: number;
+  layers: number;
+  hueStart: number;
+  hueIncrement: number;
+  ampl: number;
+  offset: number;
+  offsetIncrement: number;
+  sat: number;
+  light: number;
+  lightIncrement: number;
+}
+
+function generateWallpaperSeed(width: number): IWallpaperSeed {
+  // line segments (either few, or fluent lines (200))
+  const segments = Math.random() < 0.5 ? 1 + Math.floor(9 * Math.random()) : 200;
+  const wavelength = width / (5 + 15 * Math.random());
+
+  const layers = 3 + Math.floor(10 * Math.random());
+  const hueStart = 360 * Math.random();
+  const hueIncrement = 20 - 40 * Math.random();
+  const ampl = 0.1 * wavelength + 0.9 * wavelength * Math.random();
+  const offset = width * Math.random();
+  const offsetIncrement = width / 20 + (width / 10) * Math.random();
+  const sat = 15 + 35 * Math.random();
+  const light = 15 + 45 * Math.random();
+  const lightIncrement = Math.random() < 0.5 ? 2 + 4 * Math.random() : -(2 + 4 * Math.random());
+
+  return {
+    segments,
+    wavelength,
+    layers,
+    hueStart,
+    hueIncrement,
+    ampl,
+    offset,
+    offsetIncrement,
+    sat,
+    light,
+    lightIncrement,
+  };
+}
+
+function paintWallpaper(ctx: CanvasRenderingContext2D, seed: IWallpaperSeed, width: number, height: number): void {
+  const {
+    segments,
+    wavelength,
+    layers,
+    hueStart,
+    hueIncrement,
+    ampl,
+    offset,
+    offsetIncrement,
+    sat,
+    light,
+    lightIncrement,
+  } = seed;
+
+  ctx.fillStyle = "hsl( " + hueStart + ", " + sat + "%, " + light + "% )";
+  ctx.fillRect(0, 0, width, height);
+
+  for (let l = 0; l < layers; l++) {
+    const h = hueStart + (l + 1) * hueIncrement;
+    const s = sat;
+    const v = light + (l + 1) * lightIncrement;
+    ctx.fillStyle = "hsl( " + h + ", " + s + "%, " + v + "% )";
+    ctx.beginPath();
+    const layerOffset = offset + offsetIncrement * l;
+    const offsetY = (l + 0.5) * (height / layers);
+    const startY = offsetY + ampl * Math.sin(layerOffset / wavelength);
+    ctx.moveTo(0, startY);
+    for (let i = 0; i <= segments; i++) {
+      const x = i * (width / segments);
+      ctx.lineTo(x, startY + ampl * Math.sin((layerOffset + x) / wavelength));
+    }
+    ctx.lineTo(width, height);
+    ctx.lineTo(0, height);
+    ctx.lineTo(0, startY);
+    ctx.fill();
+  }
 }
 
 /**
