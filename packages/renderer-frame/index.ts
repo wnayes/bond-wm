ef36e36bcad010fb9bbab3b71105696ce1786581
Will Nayes <wnayes@gmail.com>
2023-04-15 13:22:14 -0500

    Fix errors from loading desktop index into frame window.

--- packages/renderer-frame/index.ts
+++ packages/renderer-frame/index.ts
@@ -1,19 +1,21 @@
 import { configureRendererStore, setPluginInstallDirectory } from "@electron-wm/renderer-shared";
 import { setupWindowComponent } from "./components/WindowSetup";
 
-setPluginInstallDirectory(__dirname);
+if (typeof window !== "undefined" && window.location.href.includes("/renderer-frame/index")) {
+  setPluginInstallDirectory(__dirname);
 
-const store = configureRendererStore();
+  const store = configureRendererStore();
 
-// Debug code, remove eventually.
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).store = store;
-// store.subscribe(() => {
-//   console.log(store.getState());
-// });
+  // Debug code, remove eventually.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).store = store;
+  // store.subscribe(() => {
+  //   console.log(store.getState());
+  // });
 
-const container = document.getElementById("content");
-if (!container) {
-  throw new Error("Missing container element in frame");
+  const container = document.getElementById("content");
+  if (!container) {
+    throw new Error("Missing container element in frame");
+  }
+  setupWindowComponent(container, store);
 }
-setupWindowComponent(container, store);
