77f3893b96bba1829440ff13ef0570f8aae9bb9b
Will Nayes <wnayes@gmail.com>
2023-04-30 16:32:17 -0500

    Fix cwd to ts-node register calls.

--- apps/wm/index.js
+++ apps/wm/index.js
@@ -1,6 +1,6 @@
 // Bootstraps ts-node and loads the TypeScript main.ts entrypoint.
 require("ts-node").register({
-    cwd: "./apps/wm",
+    cwd: __dirname,
     project: "./tsconfig.json"
 });
 require("./main.ts");
