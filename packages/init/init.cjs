ab6db104ede617022e54a82e0b15cf10005475a4
Will Nayes <wnayes@gmail.com>
2023-12-15 15:10:27 -0600

    Stubbed out init package.

---
+++ packages/init/init.cjs
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+"use strict";
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+// Stubbing this out for now, not sure what this will do exactly yet.
+
+// const { dirname, join } = require("node:path");
+// const { env } = require("node:process");
+// const cp = require("child_process");
+
+// const reactConfigPath = dirname(require.resolve("@electron-wm/react-config"));
+
+// console.log(getXDGHome());
+// console.log(reactConfigPath);
+
+// function getXDGHome() {
+//   let XDG_CONFIG_HOME = env["XDG_CONFIG_HOME"];
+//   if (!XDG_CONFIG_HOME) {
+//     const HOME = env["HOME"] || "~";
+//     XDG_CONFIG_HOME = join(HOME, ".config");
+//   }
+//   return XDG_CONFIG_HOME;
+// }
