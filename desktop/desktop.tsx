34d8d0a2583c577dff5874a0e79e65999e5d9bed
Will Nayes <wnayes@gmail.com>
2024-09-29 15:23:59 -0500

    0.5.0 release changes

--- desktop/desktop.tsx
+++ desktop/desktop.tsx
@@ -1,17 +1,36 @@
 import React from "react";
-import { Desktop, ErrorDisplay, WorkArea, ThemeContextProvider, useScreenIndex } from "@bond-wm/react";
+import { Desktop, ErrorDisplay, WorkArea, ThemeContextProvider, useScreenIndex, ChildWindow } from "@bond-wm/react";
+import { StartMenu, StartMenuApplicationList, StartMenuButton } from "@bond-wm/react-start-menu";
 import { ErrorBoundary } from "react-error-boundary";
 import { DesktopEntries } from "@bond-wm/react-desktop-entries";
 import { Taskbar, TagList, TaskList, SystemTray, Clock, LayoutIndicator } from "@bond-wm/react-taskbar";
 import { Wallpaper } from "@bond-wm/react-wallpaper";
 import { MyTheme } from "../theme";
 
+const TaskbarHeight = 20;
+
 export default () => {
   const screenIndex = useScreenIndex();
   return (
     <ThemeContextProvider theme={MyTheme}>
       <Desktop>
-        <Taskbar height={20}>
+        <Taskbar height={TaskbarHeight}>
+          {screenIndex === 0 && (
+            <StartMenuButton>
+              {() => (
+                <ChildWindow
+                  alwaysOnTop
+                  autoFocus
+                  position={{ x: 0, y: TaskbarHeight }}
+                  size={{ width: 300, height: screen.height / 2 }}
+                >
+                  <StartMenu>
+                    <StartMenuApplicationList groupBy="all" />
+                  </StartMenu>
+                </ChildWindow>
+              )}
+            </StartMenuButton>
+          )}
           <TagList />
           <TaskList />
           {screenIndex === 0 && <SystemTray />}
