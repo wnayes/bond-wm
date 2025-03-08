20940cfe8324f683aa0ea73e580dd0baa18f9467
Will Nayes <wnayes@gmail.com>
2025-03-07 22:13:18 -0600

    TypeScript 5.8 update

--- apps/wm/base64.ts
+++ apps/wm/base64.ts
@@ -1,9 +1,9 @@
 import { base64 } from "rfc4648";
 
-export function encodeArrayBufferToBase64(buffer: ArrayBuffer): string {
+export function encodeArrayBufferToBase64(buffer: ArrayBufferLike): string {
   return base64.stringify(new Uint8Array(buffer));
 }
 
-export function decodeBase64ToArrayBuffer(str: string): ArrayBuffer {
+export function decodeBase64ToArrayBuffer(str: string): ArrayBufferLike {
   return base64.parse(str).buffer;
 }
